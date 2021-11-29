412b7faba52c14dc75f63d2ec8021dc31a0f0ab6
오현화 <meke@nate.com>
2021-11-30 00:30:03 +0900

    Add @Before method

--- test_02/test/iloveyouboss/ProfileTest.java
+++ test_02/test/iloveyouboss/ProfileTest.java
@@ -1,16 +1,31 @@
 package iloveyouboss;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * ProfileTest 에 속한 각각의 테스트는 새로운 ProfileTest 인스턴스에서 @Before 메서드 호출 후 실행됨.
+ */
 public class ProfileTest {
+
+  private Profile profile;
+  private BooleanQuestion question;
+  private Criteria criteria;
+
+  @Before
+  public void create() {
+    profile = new Profile("Bull Hockey, Inc.");
+    question = new BooleanQuestion(1, "Got bonuses?");
+    criteria = new Criteria();
+  }
 
   @Test
   public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
-    Profile profile = new Profile("Bull Hockey, Inc.");
-    Question question = new BooleanQuestion(1, "Got bonuses?");
+//    Profile profile = new Profile("Bull Hockey, Inc.");
+//    Question question = new BooleanQuestion(1, "Got bonuses?");
     Answer profileAnswer = new Answer(question, Bool.FALSE);
     profile.add(profileAnswer);
     Criteria criteria = new Criteria();
@@ -25,8 +40,8 @@ public class ProfileTest {
 
   @Test
   public void matchAnswersTrueForAnyDontCareCriteria() {
-    Profile profile = new Profile("Bull Hockey, Inc.");
-    Question question = new BooleanQuestion(1, "Got milk?");
+//    Profile profile = new Profile("Bull Hockey, Inc.");
+//    Question question = new BooleanQuestion(1, "Got milk?");
     Answer profileAnswer = new Answer(question, Bool.FALSE);
     profile.add(profileAnswer);
     Criteria criteria = new Criteria();

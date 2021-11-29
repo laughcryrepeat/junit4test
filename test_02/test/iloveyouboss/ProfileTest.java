a234ff849ba3b859b0fb7b7ad7daad018f5e221c
오현화 <meke@nate.com>
2021-11-30 00:38:04 +0900

    Refactor inline

--- test_02/test/iloveyouboss/ProfileTest.java
+++ test_02/test/iloveyouboss/ProfileTest.java
@@ -24,14 +24,9 @@ public class ProfileTest {
 
   @Test
   public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
-//    Profile profile = new Profile("Bull Hockey, Inc.");
-//    Question question = new BooleanQuestion(1, "Got bonuses?");
-    Answer profileAnswer = new Answer(question, Bool.FALSE);
-    profile.add(profileAnswer);
+    profile.add(new Answer(question, Bool.FALSE));
     Criteria criteria = new Criteria();
-    Answer criteriaAnswer = new Answer(question, Bool.TRUE);
-    Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
-    criteria.add(criterion);
+    criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.MustMatch));
 
     boolean matches = profile.matches(criteria);
 
@@ -40,15 +35,9 @@ public class ProfileTest {
 
   @Test
   public void matchAnswersTrueForAnyDontCareCriteria() {
-//    Profile profile = new Profile("Bull Hockey, Inc.");
-//    Question question = new BooleanQuestion(1, "Got milk?");
-    Answer profileAnswer = new Answer(question, Bool.FALSE);
-    profile.add(profileAnswer);
+    profile.add(new Answer(question, Bool.FALSE));
     Criteria criteria = new Criteria();
-    Answer criteriaAnswer = new Answer(question, Bool.TRUE);
-    Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
-
-    criteria.add(criterion);
+    criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare));
 
     boolean matches = profile.matches(criteria);
 
